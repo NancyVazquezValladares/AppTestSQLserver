b0a9ffad8e7384a73382abbc590ec8a64673ee85
NancyVazquezValladares <113449039+NancyVazquezValladares@users.noreply.github.com>
2023-05-16 22:35:57 -0600

    Add files via upload

---
+++ appTestSQLserver/appTestSQLserver/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appTestSQLServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.
+
+            string consulta = "INSERT INTO Region (RegioID,RegionDescription) VALUES(5,'Centro')";
+            conexion.EjecutaConsulta(consulta);
+
+
+            string consulta = "select * from region order by RegionID";
+            DataTable dt = conexion.Selecciona(consulta);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conexion.Conectar();
+
+            conexion.Desconectar();
+        }
+
+        private void CargarDatos()
+        {
+            string consulta = 
+        }
+
+        private void BtnBorrar_Click(object sender, EventArgs e)
+        {
+            string id = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            string consulta = "delete from region where regionid="+id;
+
+            conexion.EjecutaConsulta(consulta);
+        }
+    }
+}
---
+++ appTestSQLserver/appTestSQLserver/conexion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appTestSQLServer
+{
+    internal class conexion
+    {
+        string server = "localhost\\SQLEXPRESS01";
+        string user = "sa";
+        string password = "fime23";
+        string database = "NORTWND";
+        string port = "1433";
+
+        public static void Conectar()
+        {
+            string cadena = "Server="+server+";Database="+database+";User Id="+user+";Password="+password+";";
+            SqlConnection cnx = new SqlConnection(cadena);
+            cnx.Open();
+
+
+        }
+
+        public static void Desconectar()
+        {
+            cnx.Close();
+             
+        }
+
+
+        public static DataTable Selecciona(string consulta)
+        {
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(consulta, cnx);
+            da.Fill(ds,"datos");
+            Desconectar();
+            return ds.Tables["datos"];
+        }
+
+        public static int EjecutaConsulta(string consulta)
+        {
+            Conectar();
+            SqlCommand c = new SqlCommand(consulta, cnx);
+            int filasAfectadas = c.ExecuteNonQuery();
+            Desconectar();
+            return filasAfectadas;
+        }
+    }
+}
